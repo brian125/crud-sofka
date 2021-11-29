ba63db148c4abc15db7afdeae44654b790c4920a
Brayan Silva <brayan_silva82151@elpoli.edu.co>
2021-11-29 09:05:40 -0500

    API

---
+++ src/main/java/co/com/sofka/crud/TodoRepository.java
@@ -0,0 +1,11 @@
+package co.com.sofka.crud;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends CrudRepository<Todo, Long> {
+
+
+
+}
